0151c7c2e7f177d1a179e0b64b0f0684f1c48d0f
Wesley Kerr <wesley.n.kerr@gmail.com>
2013-06-30 12:31:18 -0700

    initial connection to DB

--- Gemfile
+++ Gemfile
@@ -4,8 +4,5 @@ gem "json"
 gem "sinatra"
 gem "sinatra-contrib"
 gem 'haml'
-gem 'httparty'
-gem 'nokogiri'
-gem 'damerau-levenshtein', "~>1.0.2"
-gem 'mysql2'
-gem 'couchrest'
+gem 'data_mapper'
+gem 'dm-mysql-adapter'
--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'rubygems' # skip this line in ruby 1.9 and later
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'yaml'
 require 'haml'
+require 'data_mapper'
 
 require 'routes/init.rb'
+
+config_obj = YAML::load( "#{File.expand_path(__FILE__)}../config/steamrecommender.yml" )
+user = config_obj['mysql_user']
+pass = config_obj['mysql_password']
+
+DataMapper.setup(:default, "mysql://#{user}:#{pass}@mysql.seekerr.com/game_recommender")
 
 get '/' do
   haml :index

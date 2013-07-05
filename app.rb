2b44d4df98783a9300998385956ab3b573c0f495
Wesley Kerr <wesley.n.kerr@gmail.com>
2013-07-05 12:47:24 -0700

    required code and changed models.rb to match naming convention

--- app.rb
+++ app.rb
@@ -3,15 +3,9 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'yaml'
 require 'haml'
-require 'data_mapper'
 
+require 'models/init.rb'
 require 'routes/init.rb'
-
-config_obj = YAML::load( "#{File.expand_path(__FILE__)}../config/steamrecommender.yml" )
-user = config_obj['mysql_user']
-pass = config_obj['mysql_password']
-
-DataMapper.setup(:default, "mysql://#{user}:#{pass}@mysql.seekerr.com/game_recommender")
 
 get '/' do
   haml :index

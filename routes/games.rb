1bab7025fb3cafbd0013ebd8467b50a4c032c8da
Wesley Kerr <wesley.n.kerr@gmail.com>
2013-09-22 15:32:07 -0700

    Added ability to sort games and genres

--- routes/games.rb
+++ routes/games.rb
@@ -3,7 +3,11 @@ require 'will_paginate/data_mapper'
 
 get '/games' do
   page = params[:page] || 1
-  @games = Game.paginate(:app_type => 'game', :order => [:appid.asc],  :page => page, :per_page => 30)
+  order_list = [:appid.asc]
+  if params[:order]
+    order_list = [ params[:order].to_sym.desc, :appid.asc ]
+  end
+  @games = Game.paginate(:app_type => 'game', :order => order_list,  :page => page, :per_page => 30)
   haml :games
 end
 
--- routes/genres.rb
+++ routes/genres.rb
@@ -9,9 +9,13 @@ end
 
 get '/genres/:genre' do
   page = params[:page] || 1
+  order_list = [:appid.asc]
+  if params[:order]
+    order_list = [ params[:order].to_sym.desc, :appid.asc ]
+  end
   @genre = Genre.get(params[:genre])
   @title = @genre.name
-  @games = @genre.games.paginate(:order => [:appid.asc], :page => page, :per_page => 30)
+  @games = @genre.games.paginate(:app_type => 'game', :order => order_list,  :page => page, :per_page => 30)
   haml :genre
 end
 
